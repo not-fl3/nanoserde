6575cd8ada584420928e1a123be64ef0dfd11a38
kangalioo <jannik.a.schaper@web.de>
2022-08-22 15:29:43 +0200

    Add test for JSON control character escaping rules

--- tests/json.rs
+++ tests/json.rs
@@ -656,3 +656,12 @@ fn tuple_struct2() {
 
     assert!(test == test_deserialized);
 }
+
+#[test]
+fn control_characters() {
+    let string = ('\0'..=' ').collect::<String>();
+    // Generated with serde_json
+    let escaped = r#""\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f ""#;
+
+    assert_eq!(SerJson::serialize_json(&string), escaped);
+}
