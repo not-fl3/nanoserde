0517a262bf4ad4566d1b197d137100889e80cb78
hoang <htbn.hoang@gmail.com>
2023-07-14 00:33:12 +0700

    Update test for `TomlParser`

--- tests/toml.rs
+++ tests/toml.rs
@@ -4,10 +4,11 @@ use nanoserde::TomlParser;
 fn de_toml() {
     let toml_str = r#"
 [[array]]
-name = "a"
+# a
+name = "a" #aasdf
 
 [[array]]
-name = "b"
+name = "b#asdf"
 data = 123
 
 [[array]]
@@ -24,7 +25,7 @@ hmm = "b"
     let toml = TomlParser::parse(toml_str).unwrap();
 
     assert_eq!(toml["array"].arr()[0]["name"].str(), "a");
-    assert_eq!(toml["array"].arr()[1]["name"].str(), "b");
+    assert_eq!(toml["array"].arr()[1]["name"].str(), "b#asdf");
     assert_eq!(toml["array"].arr()[1]["data"].num(), 123.);
 
     assert_eq!(toml["other_array"].arr()[0]["hmm"].str(), "a");
