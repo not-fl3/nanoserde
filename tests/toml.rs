2d0b502c20b7b6cb158334c6fff67a47706dd3bf
hoang <hoang@sonigames.com>
2023-07-16 01:16:40 +0700

    Add some test case from toml.io

--- tests/toml.rs
+++ tests/toml.rs
@@ -31,3 +31,25 @@ hmm = "b"
     assert_eq!(toml["other_array"].arr()[0]["hmm"].str(), "a");
     assert_eq!(toml["other_array"].arr()[1]["hmm"].str(), "b");
 }
+
+#[test]
+fn comment() {
+    let data = r#"
+    # This is a full-line comment
+    key = "value"  # This is a comment at the end of a line
+    another = " # This is not a comment"
+    "#;
+
+    let toml = TomlParser::parse(data).unwrap();
+    assert_eq!(toml["key"].str(), "value");
+    assert_eq!(toml["another"].str(), " # This is not a comment");
+}
+
+#[test]
+fn key_without_value() {
+    let data = r#"
+    key = # INVALID
+    "#;
+
+    assert!(TomlParser::parse(data).is_err());
+}
