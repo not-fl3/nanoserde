5be4444a91515437e61cd48d35f1e9a36d6ed444
kirk <knickish@gmail.com>
2023-07-19 11:11:58 +0000

    add RON serde testing to mixed test

--- tests/ser_de.rs
+++ tests/ser_de.rs
@@ -1,10 +1,10 @@
-use nanoserde::{DeBin, DeJson, SerBin, SerJson};
+use nanoserde::{DeBin, DeJson, DeRon, SerBin, SerJson, SerRon};
 
 use std::collections::HashMap;
 
 #[test]
 fn ser_de() {
-    #[derive(DeBin, SerBin, DeJson, SerJson, PartialEq)]
+    #[derive(DeBin, SerBin, DeJson, SerJson, DeRon, SerRon, PartialEq, Debug)]
     pub struct Test {
         pub a: i32,
         pub b: f32,
@@ -28,9 +28,13 @@ fn ser_de() {
 
     let bytes = SerBin::serialize_bin(&test);
     let test_deserialized = DeBin::deserialize_bin(&bytes).unwrap();
-    assert!(test == test_deserialized);
+    assert_eq!(test, test_deserialized);
 
     let bytes = SerJson::serialize_json(&test);
     let test_deserialized = DeJson::deserialize_json(&bytes).unwrap();
-    assert!(test == test_deserialized);
+    assert_eq!(test, test_deserialized);
+
+    let bytes = SerRon::serialize_ron(&test);
+    let test_deserialized = DeRon::deserialize_ron(&bytes).unwrap();
+    assert_eq!(test, test_deserialized);
 }
