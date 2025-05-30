aca635b206c40990b2983a46e59ea32c149511a3
Rowan Hart <rowanbhart@gmail.com>
2025-05-30 00:41:49 -0700

    Remove feature gating for use statements in shared

--- derive/src/shared.rs
+++ derive/src/shared.rs
@@ -1,12 +1,8 @@
 #![cfg(any(feature = "json", feature = "ron", feature = "binary"))]
 
-#[cfg(any(feature = "json", feature = "binary"))]
-use alloc::{format, string::ToString, vec::Vec};
-
-use alloc::string::String;
-
-#[cfg(any(feature = "binary", feature = "json"))]
 use crate::parse::{Enum, Struct};
+use alloc::string::String;
+use alloc::{format, string::ToString, vec::Vec};
 
 macro_rules! l {
     ($target:ident, $line:expr) => {
